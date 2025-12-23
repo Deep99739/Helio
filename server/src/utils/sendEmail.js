bbc42e01393467bb172c955e01249ac90a0fbfae
deep99739 <btech10645.22@bitmesra.ac.in>
2025-12-23 08:20:34 +0530

    fix smtp connection timeout

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -4,7 +4,9 @@ const logger = require('./logger');
 const sendEmail = async (options) => {
     // secure: true for 465, false for other ports
     const transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false, // true for 465, false for other ports
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
