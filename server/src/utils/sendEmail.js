47dd8e287f4ddd77645859df60e8a170f64e993b
deep99739 <btech10645.22@bitmesra.ac.in>
2025-12-23 08:27:58 +0530

    update smtp to port 465 ssl

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -5,12 +5,15 @@ const sendEmail = async (options) => {
     // secure: true for 465, false for other ports
     const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
-        port: 587,
-        secure: false, // true for 465, false for other ports
+        port: 465,
+        secure: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
-        }
+        },
+        connectionTimeout: 10000, // 10 seconds
+        logger: true,
+        debug: true
     });
 
     const message = {
