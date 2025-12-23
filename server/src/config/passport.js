e7aa182d90b8f4471c8abd9aea60c24372bb362c
deep99739 <btech10645.22@bitmesra.ac.in>
2025-12-23 08:05:46 +0530

    fix passport callback url

--- server/src/config/passport.js
+++ server/src/config/passport.js
@@ -2,10 +2,13 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/User');
 
+const baseUrl = process.env.RENDER_EXTERNAL_URL || 'http://localhost:5000';
+
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/auth/google/callback"
+    callbackURL: `${baseUrl}/api/auth/google/callback`,
+    proxy: true
 },
     async (accessToken, refreshToken, profile, done) => {
         try {
